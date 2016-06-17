9d9b844cbe5fe2667fc5170fd4c22aa52b30ad86
Roland Willwerth <rwillwerth@stitchfix.com>
2016-06-17 13:38:38 -0700

    Added process.argv

--- watcher.js
+++ watcher.js
@@ -7,8 +7,13 @@
  * Visit http://www.pragmaticprogrammer.com/titles/jwnode for more book information.
 ***/
 
-const fs = require('fs');
-fs.watch('target.txt', function() {
-  console.log("File 'target.txt' just changed!");
-});
-console.log("Now watching target.txt for changes...");
+    const
+      fs = require('fs'),
+      filename = process.argv[2];
+    if (!filename) {
+      throw Error("A file to watch must be specified!");
+    }
+    fs.watch(filename, function() {
+      console.log("File " + filename + " just changed!");
+    });
+    console.log("Now watching " + filename + " for changes...");
