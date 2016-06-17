780c5751c3c707ed3b43e9c51579c6d72a6e2c98
Roland Willwerth <rwillwerth@stitchfix.com>
2016-06-17 14:47:02 -0700

    Added child_process .spawn

--- watcher.js
+++ watcher.js
@@ -7,13 +7,18 @@
  * Visit http://www.pragmaticprogrammer.com/titles/jwnode for more book information.
 ***/
 
-    const
-      fs = require('fs'),
-      filename = process.argv[2];
-    if (!filename) {
-      throw Error("A file to watch must be specified!");
-    }
-    fs.watch(filename, function() {
-      console.log("File " + filename + " just changed!");
-    });
-    console.log("Now watching " + filename + " for changes...");
+"use strict";
+const
+  fs = require('fs'),
+  spawn = require('child_process').spawn,
+  filename = process.argv[2];
+
+if (!filename) {
+  throw Error("A file to watch must be specified!");
+}
+
+fs.watch(filename, function() {
+  let ls = spawn('ls', ['-lh', filename]);
+  ls.stdout.pipe(process.stdout);
+});
+console.log("Now watching " + filename + " for changes...");
